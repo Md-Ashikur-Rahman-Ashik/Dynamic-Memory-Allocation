bea8b3a4c84400316c41990406c5b132a253e802
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-27 23:53:35 +0600

    Exploring dynamic array

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
-    
+    // int integerArray[5];
+
+    int *integerArray = new int[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cin >> integerArray[i];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cout << integerArray[i] << " ";
+    }
 
     return 0;
 }
